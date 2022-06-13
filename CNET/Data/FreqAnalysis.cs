f817cf7b6669bb142d79cbc984e84931a9fc4bef
ITC <teofil.kvak@yandex.ru>
2022-06-13 13:39:36 +0200

    Data Class

---
+++ CNET/Data/FreqAnalysis.cs
@@ -0,0 +1,22 @@
+﻿namespace Data
+{
+    public static class FreqAnalysis
+    {
+        public static Dictionary<string, int> FreqAnalysisFromString(string input)
+        {
+            //var result = new Dictionary<string,int>();
+            throw new NotImplementedException();
+        }
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrl(string url)
+        {
+            HttpClient httpClient = new ();
+            var content = await httpClient.GetStringAsync(url);
+            return FreqAnalysisFromString(content);
+        }
+        public static Dictionary<string, int> FreqAnalysisFromFile(string file)
+        {
+            string content = File.ReadAllText(file);
+            return FreqAnalysisFromString(content);
+        }
+    }
+}
