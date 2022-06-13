42c75e1072e77da75cf087e34c3ad0a8005454a7
ITC <teofil.kvak@yandex.ru>
2022-06-13 16:18:55 +0200

    Linq

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -2,35 +2,83 @@
 using Model;
 using Data;
 Console.WriteLine("Hello, World!");
-string booksdir = @"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\Books";
 
-var files = Directory.EnumerateFiles(booksdir, "*.txt");
-foreach (var file in files)
+int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
+
+//WHERE - filtracia
+var resNums = numbers.Where(n => n > 0 && n < 100);
+
+resNums.ToList().ForEach(x => Console.WriteLine(x));
+foreach (var item in resNums)
 {
-    var result = FreqAnalysis.FreqAnalysisFromFile(file);
-    Console.WriteLine(new FileInfo(file).Name);
-
-    //vytlacit 10 najcastejsich
-
-    var ordered = result.OrderByDescending(a => a.Value).Take(10);
-    foreach (var item in ordered)
-    {
-        Console.WriteLine($"{item.Key} {item.Value}");
-    }
-    Console.WriteLine();
-
-
+    Console.WriteLine(item);
 }
 
-FAResult fAResult = new FAResult()
+Console.WriteLine();
+
+//ORDERBY
+var resOrder = numbers.OrderBy(x => x);
+
+//AGREGACIA
+var resMax = numbers.Max();//  .Max(x => x);
+var resSum = numbers.Sum();
+
+//TAKE SKIP
+var resTake = numbers.Take(2);
+var resTakeWhile = numbers.TakeWhile(x => x > 0);
+Console.WriteLine(resTakeWhile);
+
+//SELECT - transformacia
+Console.WriteLine(numbers.Select(x => Math.Abs(x)).ToList());
+
+Console.WriteLine(numbers.Where(x => x > 0).Count());
+
+Console.WriteLine(numbers.Distinct().OrderBy(n => n).Skip(1).SkipLast(1).Average());
+Console.WriteLine(numbers.Where(a => a % 2 == 0).Count());
+Console.WriteLine(numbers.Where(a => a % 2 != 0).Count());
+
+
+void print(object colection)
 {
-    Source = "file",
-    SourceType = SourceType.FILE
-};
+    foreach (object o in colection.)
+    {
+        Console.WriteLine(o)
+    }
+}
 
-Dictionary<string, int> x = Data.FreqAnalysis.FreqAnalysisFromFile(@"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\Books\alice.txt");
 
-Console.WriteLine(fAResult);
+static void FrequentWords()
+{
+    string booksdir = @"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\Books";
 
-fAResult.Words = new Dictionary<string, int>();
-Console.WriteLine(fAResult);
+    var files = Directory.EnumerateFiles(booksdir, "*.txt");
+    foreach (var file in files)
+    {
+        var result = FreqAnalysis.FreqAnalysisFromFile(file);
+        Console.WriteLine(new FileInfo(file).Name);
+
+        //vytlacit 10 najcastejsich
+
+        var ordered = result.OrderByDescending(a => a.Value).Take(10);
+        foreach (var item in ordered)
+        {
+            Console.WriteLine($"{item.Key} {item.Value}");
+        }
+        Console.WriteLine();
+
+
+    }
+
+    FAResult fAResult = new FAResult()
+    {
+        Source = "file",
+        SourceType = SourceType.FILE
+    };
+
+    Dictionary<string, int> x = Data.FreqAnalysis.FreqAnalysisFromFile(@"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\Books\alice.txt");
+
+    Console.WriteLine(fAResult);
+
+    fAResult.Words = new Dictionary<string, int>();
+    Console.WriteLine(fAResult);
+}
