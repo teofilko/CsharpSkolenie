7f11c52a57b89fdcb8b1c07201a881455c567a66
ITC <teofil.kvak@yandex.ru>
2022-06-13 11:49:42 +0200

    uprava FAResult

--- CNET/Model/FAResult.cs
+++ CNET/Model/FAResult.cs
@@ -1,10 +1,32 @@
 ﻿namespace Model
 {
+    public enum SourceType
+    {
+        URL,
+        FILE
+    }
     /// <summary>
     /// Vysledok frekvencnej analyzy pre jeden zdroj ( subor ci Url)
     /// </summary>
     public class FAResult
     {
+        
+        /// <summary>
+        /// Zdroj textu
+        /// </summary>
+        public string Source { get; set; }
+        public SourceType SourceType { get; set; }
+
+        /// <summary>
+        /// Vysledna frekvencna analyza slov
+        /// </summary>
+        public Dictionary<string,int> Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Source} {Words.Count}";
+        }
+
 
     }
 }
