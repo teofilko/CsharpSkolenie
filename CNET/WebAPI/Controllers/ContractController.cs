9d626e14d9127f9d5f9b3dabe384e8a4b0cf73c2
ITC <teofil.kvak@yandex.ru>
2022-06-15 14:52:20 +0200

    web API

---
+++ CNET/WebAPI/Controllers/ContractController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<Model.Contract> GetContract()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\PersonDataset\dataset.xml");
+            return dataset.SelectMany(p=>p.Contracts);
+        }
+    }
+}
---
+++ CNET/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public List<Person> GetPeople()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\PersonDataset\dataset.xml");
+            return dataset;
+        }
+
+        [HttpGet("GetByEmail/{email}")]
+        public Person? GetByEmail(string email)
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\StudentEN\source\repos\CsharpSkolenie\CNET\PersonDataset\dataset.xml");
+            var persons = dataset.Where(p => p.Email.ToUpper() == email.ToUpper()).FirstOrDefault();
+            
+                return persons;
+            
+        }
+    }
+}
